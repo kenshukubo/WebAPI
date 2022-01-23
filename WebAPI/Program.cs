0a7c2ef1821fb4664a4a3f9e3276075dbfdfa4bd
kenshu <kensyu124logic@yahoo.co.jp>
2022-01-23 19:55:57 +0900

    UseCors追加

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -26,6 +26,8 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
+app.UseCors(options => options.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
