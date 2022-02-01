bb39bfd829bc4150fd03a0f5cfdb3f602e705b0b
kenshu <kensyu124logic@yahoo.co.jp>
2022-02-01 09:30:35 +0900

    update

--- WebAPI/Controllers/EmployeeController.cs
+++ WebAPI/Controllers/EmployeeController.cs
@@ -163,29 +163,29 @@ namespace WebAPI.Controllers
         //}
 
 
-        //[Route("GetAllDepartmentNames")]
-        //public JsonResult GetAllDepartmentNames()
-        //{
-        //    string query = @"
-        //            select DepartmentName from dbo.Department
-        //            ";
-        //    DataTable table = new DataTable();
-        //    string sqlDataSource = _configuration.GetConnectionString("EmployeeAppCon");
-        //    SqlDataReader myReader;
-        //    using (SqlConnection myCon = new SqlConnection(sqlDataSource))
-        //    {
-        //        myCon.Open();
-        //        using (SqlCommand myCommand = new SqlCommand(query, myCon))
-        //        {
-        //            myReader = myCommand.ExecuteReader();
-        //            table.Load(myReader); ;
+        [Route("GetAllDepartmentNames")]
+        public JsonResult GetAllDepartmentNames()
+        {
+            string query = @"
+                    select DepartmentName from dbo.Department
+                    ";
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("EmployeeAppCon");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader); ;
 
-        //            myReader.Close();
-        //            myCon.Close();
-        //        }
-        //    }
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
 
-        //    return new JsonResult(table);
-        //}
+            return new JsonResult(table);
+        }
     }
 }
